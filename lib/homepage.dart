f90a8385bc7a4473fc52779a2b37b631571a1c45
Mihail-Trofimov <89457417+thq919@users.noreply.github.com>
2021-11-30 13:55:39 +1000

    ебать рефактор лютый

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,7 +8,6 @@ import 'YT/yt_api_handler.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
-  late YoutubeExplode player;
   var title;
 
   @override
@@ -16,138 +15,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  MainPlayer player = MainPlayer();
-  SearchList? videoSearchList;
-  bool isSearchListExist = false;
-  String? searchQue;
-  late WidgetList list;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Wrap(children: [
-        buildTextFieldSearch(),
-        if (searchQue != null) buildTree(),
-      ]),
-    );
+    return Scaffold(appBar: AppBar(), body: WidgetList());
   }
+}
 
-  TextField buildTextFieldSearch() {
-    return TextField(onChanged: (value) {
-      setState(() {
-        searchQue = value;
-      });
-    });
-  }
+class WidgetList extends StatefulWidget {
+  WidgetList({Key? key}) : super(key: key);
 
-  FutureBuilder<SearchList?> buildTree() {
-    return FutureBuilder(
-      future: fillTheList(searchQue),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData) {
-          searchQue = null;
-          list = WidgetList(player: player, search: snapshot.data!);
-          return list;
-        }
-        if (snapshot.hasError) {
-          return Text('ощибка');
-        } else
-          return CircularProgressIndicator();
-      },
-    );
-  }
+  @override
+  State<WidgetList> createState() => _WidgetListState();
+}
 
-  Future<SearchList?> fillTheList(String? value) async {
-    if (value!.isEmpty) {
+class _WidgetListState extends State<WidgetList> {
+  MainPlayer player = MainPlayer();
+  late SearchList searchList;
+  String? searchQue;
+
+  Future<SearchList?> fillTheList(String? searchQuery) async {
+    if (searchQuery!.isEmpty) {
       Future.delayed(const Duration(milliseconds: 500));
     }
-    if (value.isNotEmpty) {
-      searchQue = value;
-      return player.searchAudio(value);
+    if (searchQuery.isNotEmpty) {
+      searchQue = searchQuery;
+      return player.searchAudio(searchQuery);
     }
-  }
-}
-
-class WidgetList extends StatelessWidget {
-  late MainPlayer mainPlayer;
-  late SearchList searchList;
-
-  WidgetList({Key? key, required MainPlayer player, required SearchList search})
-      : super(key: key) {
-    mainPlayer = player;
-    searchList = search;
   }
 
   @override
   Widget build(BuildContext context) {
-    //  ErrorSummary('Vertical viewport was given unbounded height.'),
-    //  ErrorDescription(
-    //  'Viewports expand in the scrolling direction to fill their container. '
-    //  'In this case, a vertical viewport was given an unlimited amount of '
-    //  'vertical space in which to expand. This situation typically happens '
-    //  'when a scrollable widget is nested inside another scrollable widget.',
-    //  ),
-    //  ErrorHint(
-    //  'If this widget is always nested in a scrollable widget there '
-    //  'is no need to use a viewport because there will always be enough '
-    //  'vertical space for the children. In this case, consider using a '
-    //  'Column instead. Otherwise, consider using the "shrinkWrap" property '
-    //  '(or a ShrinkWrappingViewport) to size the height of the viewport '
-    //  'to the sum of the heights of its children.',
-    return ListView.builder(
-        itemCount: 10,
-        shrinkWrap: true,
-        itemBuilder: (BuildContext context, int position) {
-          return ListTile(
-            onTap: () =>
-                {mainPlayer.playAudio(searchList.elementAt(position).url)},
-            title: Video_single_shelf(searchList.elementAt(position)),
-          );
+    return Wrap(children: [
+      TextField(onChanged: (value) {
+        setState(() {
+          searchQue = value;
         });
+      }),
+      searchQue != null
+          ? FutureBuilder(
+              future: fillTheList(searchQue),
+              builder: (BuildContext context, AsyncSnapshot searchListFuture) {
+                if (searchListFuture.hasData) {
+                  searchList = searchListFuture.data;
+                  return ListView.builder(
+                      itemCount: 10,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int position) {
+                        return ListTile(
+                          onTap: () => {
+                            player.playAudio(searchList.elementAt(position).url)
+                          },
+                          title: Video_single_shelf(
+                              searchList.elementAt(position)),
+                        );
+                      });
+                }
+                if (searchListFuture.hasError) {
+                  return Text('ощибка');
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            )
+          : Text('Введите что нибудь... сверху :)')
+    ]);
   }
 }
-
-class MyController extends ScrollController {}
-// class LoginPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: Colors.black54,
-//         appBar: AppBar(
-//           title: Text('TeleMatch Login'),
-//         ),
-//         body: Padding(
-//           padding: EdgeInsets.all(16.0),
-//           child: Center(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text(
-//                     'Telematcher'),
-//                 Container(
-//                   height: 20,
-//                 ),
-//                 Container(
-//                   child: TextField(),
-//                   width: (500),
-//                 ),
-//                 Container(
-//                   height: 20,
-//                 ),
-//                 Container(
-//                   child: TextField(),
-//                   width: (500),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ));
-//   }
-// }
-// Как меня заебала эта срань просто пиздец, я не понимаю где я нахожусб
